015e22d1daf3ad139ff31ab52da98c88df86aadf
Clifford Gardner <cliffordgardner9@gmail.com>
2023-07-27 14:16:38 -0500

    git add -A, then commit.

---
+++ js/external1.js
@@ -0,0 +1,5 @@
+console.log("Hey from outside");
+alert("Welcome to my house of Pain");
+const rep = prompt("What's your favorite color");
+alert("Awesome " + rep + " is my favorite color too!");
+let cc = confirm("Hey")
