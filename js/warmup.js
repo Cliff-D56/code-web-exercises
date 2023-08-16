0ee814eb92873804262fa416fdf375f1e39786f6
Clifford Gardner <cliffordgardner9@gmail.com>
2023-08-16 16:54:42 -0500

    Dont Get Flex

--- js/warmup.js
+++ js/warmup.js
@@ -1,7 +1,24 @@
-let loguser =(x)=>{
-    while (typeof x!=="string"||!isNaN(parseInt(x))){
+// let loguser =(x,y)=>{
+//     while (typeof x!=="string"||!isNaN(parseInt(x))){
+//         alert(`Incorrect username`)
+//         x = prompt(`Reenter Username`);
+//     }
+//     return(`${x} with ${y} has longed in for the day`)
+// }
+let loguser =(object)=>{
+    while (typeof object.username!=="string"||!isNaN(parseInt(object.username))){
         alert(`Incorrect username`)
-        x = prompt(`Reenter Username`);
+        object.username = prompt(`Reenter Username`);
     }
-    alert(`${x} has longed in for the day`)
+    return(`${object.username} with ${object.email} has longed in for the day`)
+}
+let userObject ={
+    email:"cliffordgardner9@gmail.com",
+    username:"cliffordgardner9",
+    userRights:["admin","moderator","buyer","seller"],
+    userProfile:{
+        userProfileURL:"www.youtube.com",
+        userLocation:"San Antonio Texas",
+        numLikes:0
+    }
 }
