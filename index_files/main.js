fec404454dff9e944a22bb956eaac45dc0b8a45d
Clifford Gardner <cliffordgardner9@gmail.com>
2023-09-06 10:10:51 -0500

    Wednesday Update

---
+++ index_files/main.js
@@ -0,0 +1,104 @@
+"use strict"
+//IS CALLED TO CREATE THE OUTLINE FOR EACH COFFEE PRODUCT
+function renderCoffee(coffee) {
+    var html = '<ul class="coffee" id="coffee" style="list-style: none">';
+    html += '<li style="font-size: 50px" class="coffeeName">' + coffee.name + '</li>';
+    html += '<li class="coffeeRoast">' + coffee.roast + '</li>';
+    html += '</ul>';
+
+    return html;
+}
+//CREATES THE HTML CODE THAT WILL BE FILTERED INTO THE DISPLAY FOR COFFEE
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var roastSelection = document.querySelector('#roast-selection');
+
+tbody.innerHTML = renderCoffees(coffees);
+
+submitButton.addEventListener('click', updateCoffees);
+var search = document.getElementById("search")
+var roastselect = document.getElementById("roast-selection")
+//Filters Roast TODO: Filter out results based on values
+roastselect.addEventListener("change",
+    (event)=>{
+    switch (roastSelection.value){
+        case "All":
+            alert("All");
+            break;
+        case "1":
+            alert("1");
+            break;
+        case "2":
+            alert("2");
+            break;
+        case "3":
+            alert("3");
+            break;
+    }
+    })
+//Searches Coffee Input
+search.addEventListener("keyup", (event)=>{
+        let text = document.getElementById("coffee")
+        let coffeeproduct = document.getElementsByClassName("coffee")
+        let input = document.getElementById("search").value
+        input=input.toLowerCase()
+
+
+
+
+
+        let noResults = true;
+        for (let i = 0; i < coffeeproduct.length; i++) {
+            if (!coffeeproduct[i].innerHTML.toLowerCase().includes(input)) {
+                coffeeproduct[i].style.display="none";
+                continue;
+            }
+            else {
+                coffeeproduct[i].style.display="block";
+                noResults = false;
+            }
+        }
+})
+
+// document.addEventListener('keydown',hi)
+// input1.addEventListener("keydown", (event)=>{
+//     console.log(event.key)
+// })
--- js/coffee.js
+++ js/coffee.js
@@ -1,9 +1,10 @@
 // TODO: Reference your past code or write fresh code to make a book object. This object should have properties to store the book's title, the book's author, and (optionally) the genre or another property that would make sense for the object to have.
-let books={
+let books=[{
         title: "Hidden In Black Ice",
         author: "Dark",
         genre: "Drama"
-}
+    }
+]
 let createbook = function (title,author,genre){
         return{
                 title:title,
@@ -15,22 +16,26 @@ console.log(books);
 
 //TODO: Uncomment the following variable - fill in the string interpolation expressions to access the properties of your book to finish the following card.
 
+//TODO: Using JavaScript, add your finished HTML card to the DOM so our users will be able to see it on page load. We want target the innerHTML of #container.
+//TODO: From prior work or fresh in this file, let's make an array of book objects (at least three).
+let bookauthors =['Clifford','Necee','Steve'];
+let booktitles=['Throne of Glass','Terminator','God of War'];
+let bookgenres=['Romance','Action','Thriller'];
+for (let i = 0; i < booktitles.length; i++) {
+        books.push(createbook(booktitles[i],bookauthors[i], bookgenres[i]));
+}
+let cardhead = document.getElementById("container")
+console.log(books);
+let header = ()=>{cardhead.innerHTML=myCard};
+header();
 let myCard = `<div class="card">
     <div>Book Title: ${books.title}.</div>
     <div>Book Author: ${books.author}.</div>
     <div>Book Genre: ${books.genre}.</div>
 </div>`
-//TODO: Using JavaScript, add your finished HTML card to the DOM so our users will be able to see it on page load. We want target the innerHTML of #container.
-let cardhead = document.getElementById("container")
-console.log(cardhead);
-let header = ()=>{cardhead.innerHTML=myCard};
-header();
-//TODO: From prior work or fresh in this file, let's make an array of book objects (at least three).
-
-books.push(createbook("HIBC","Clifford Gardner","Romance"));
-books.forEach(function (book,i){
-
-})
+// books.forEach(function (book,i){
+//
+// })
 //TODO: Refactor your above code - our HTML needs to build three HTML cards with the information from the objects. Hint: We would want to iterate through the array and add each fresh bit of HTML into the variable of allCards set up below.
 
 let allCards = ""; //bucket to store the HTML you build in the loop
